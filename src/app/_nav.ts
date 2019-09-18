be337d78ccec02f32091478926609c38c3eaabd4
shivakrishna_donthi <sdonthi@snapfish-llc.com>
2019-09-19 00:39:32 +0530

    initial commit

---
+++ src/app/_nav.ts
@@ -0,0 +1,66 @@
+interface NavAttributes {
+  [propName: string]: any;
+}
+interface NavWrapper {
+  attributes: NavAttributes;
+  element: string;
+}
+interface NavBadge {
+  text: string;
+  variant: string;
+}
+interface NavLabel {
+  class?: string;
+  variant: string;
+}
+
+export interface NavData {
+  name?: string;
+  url?: string;
+  icon?: string;
+  badge?: NavBadge;
+  title?: boolean;
+  children?: NavData[];
+  variant?: string;
+  attributes?: NavAttributes;
+  divider?: boolean;
+  class?: string;
+  label?: NavLabel;
+  wrapper?: NavWrapper;
+}
+
+export const navItems: NavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-star',
+  },
+
+  {
+    name: 'Charts',
+    url: '/charts',
+    icon: 'icon-pie-chart',
+  },
+  {
+    name: 'Pages',
+    url: '/pages',
+    icon: 'icon-star',
+    children: [
+      {
+        name: 'Login',
+        url: '/login',
+        icon: 'icon-star',
+      },
+      {
+        name: 'Register',
+        url: '/register',
+        icon: 'icon-star',
+      },
+      {
+        name: 'Error 404',
+        url: '/404',
+        icon: 'icon-star',
+      },
+    ],
+  },
+];
